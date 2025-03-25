ca7a2879b5456351f9bd6c89a3cd2b41f7d46c46
Shiven H Patel <64271093+Shiven14@users.noreply.github.com>
2025-03-25 09:47:55 -0400

    Add files via upload

---
+++ lab4/tsh/launch.h
@@ -0,0 +1,22 @@
+/*.........................................................................*/
+/*                     LAUNCH.H ------> LAUNCH Handler                   */
+/*                     NOV '19, updated by BATOOL WAZZAN             */
+/*.........................................................................*/
+
+
+#include "synergy.h"
+
+
+char login[NAME_LEN];
+
+
+int tshsock ;
+
+
+int connectTsh(u_short) ; 
+
+
+char login[NAME_LEN];
+
+void OpShell(/*void*/) ; // added 
+
